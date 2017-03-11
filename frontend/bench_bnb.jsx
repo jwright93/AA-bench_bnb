984fdd3821d92a4e7da76b895f199d2cda02954c
Jonathon Wright <jonwright93@gmail.com>
2017-03-10 17:02:08 -0800

    Add api session ajax request"

--- frontend/bench_bnb.jsx
+++ frontend/bench_bnb.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import * as API from './util/session_api_util';
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
   ReactDOM.render(<h1>Welcome to BenchBnb</h1>, root);
+
+  window.signUp = API.signUp;
+  window.login = API.login;
+  window.logout = API.logout;
 });
---
+++ frontend/util/session_api_util.js
@@ -0,0 +1,17 @@
+export const signUp = (user) => (
+  $.ajax({method:'POST',
+  url: 'api/users',
+  data:user })
+);
+
+export const login = (user) => (
+  $.ajax({method:'POST',
+  url: 'api/session',
+  data:{user} })
+);
+
+export const logout = (user) => (
+  $.ajax({method:'DELETE',
+  url: 'api/session',
+  data:{user} })
+);
